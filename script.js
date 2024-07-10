497a35c1c5de2b25bdecd27a945a2bfd1f915b88
Johannes <johanneshaederle18092@gmail.com>
2024-07-10 23:39:58 +0200

    Fixed stuff and added logic

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("mazeCanvas");
 const ctx = canvas.getContext("2d");
 
-const cols = 25;
-const rows = 25;
+const cols = 30;
+const rows = 30;
 const cellSize = canvas.width / cols;
 
 let grid = [];
@@ -16,9 +16,81 @@ class Cell {
     this.walls = [true, true, true, true]; // top, right, bottom, left
     this.visited = false;
   }
+
+  draw() {
+    const x = this.i * cellSize;
+    const y = this.j * cellSize;
+
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 3;
+
+    if (this.walls[0]) {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x + cellSize, y);
+      ctx.stroke();
+    }
+    if (this.walls[1]) {
+      ctx.beginPath();
+      ctx.moveTo(x + cellSize, y);
+      ctx.lineTo(x + cellSize, y + cellSize);
+      ctx.stroke();
+    }
+    if (this.walls[2]) {
+      ctx.beginPath();
+      ctx.moveTo(x + cellSize, y + cellSize);
+      ctx.lineTo(x, y + cellSize);
+      ctx.stroke();
+    }
+    if (this.walls[3]) {
+      ctx.beginPath();
+      ctx.moveTo(x, y + cellSize);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
+
+    if (this.visited) {
+      ctx.fillStyle = "#fff";
+      ctx.fillRect(x, y, cellSize, cellSize);
+    }
+  }
+
+  highlight(color) {
+    const x = this.i * cellSize;
+    const y = this.j * cellSize;
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, cellSize, cellSize);
+  }
+
+  checkNeighbors() {
+    let neighbors = [];
+
+    const top = grid[index(this.i, this.j - 1)];
+    const right = grid[index(this.i + 1, this.j)];
+    const bottom = grid[index(this.i, this.j + 1)];
+    const left = grid[index(this.i - 1, this.j)];
+
+    if (top && !top.visited) {
+      neighbors.push(top);
+    }
+    if (right && !right.visited) {
+      neighbors.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbors.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbors.push(left);
+    }
+
+    if (neighbors.length > 0) {
+      const r = Math.floor(Math.random() * neighbors.length);
+      return neighbors[r];
+    } else {
+      return undefined;
+    }
+  }
 }
-
-
 
 
 
